d8d612cc2bdf66b15a55c72bd99463e37668105c
Gulzat Karimova <gulzat.karimova@gmail.com>
2018-10-29 01:26:30 -0700

    Created object-oriented class file Address-book

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+//Business  logic for AddressBook
+function AddressBook() {
+  this.contacts = []
+  this.currentId = 0
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
+  this.contacts.push(contact);
+}
+
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+AddressBook.prototype.findContact = function() {
+  for(var i = 0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id==id) {
+    return this.contact[i];
+  }
+}
+};
+return false;
+}
+
+AddressBook.prototype.deleteContact = function(id) {
+  for (var i-0; i< this.contacts.length; i++)
+  if (this.contacts[i]) {
+  if (this.contacts[i].id == id) {
+    delete this.contacts[i];
+    return true;
+  }
+}
+};
+return false;
+}
+
+//Business logic for Contacts
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName,
+  this.lastName = lastName,
+  this.phoneNumber = phoneNumber
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
