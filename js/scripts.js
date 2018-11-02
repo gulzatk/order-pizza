163b69827bed83b2abaf545721c0ea225952d0d6
Gulzat Karimova <gulzat.karimova@gmail.com>
2018-11-02 12:15:45 -0700

    It will updated the fime to add final output to scripts

--- js/scripts.js
+++ js/scripts.js
@@ -3,48 +3,56 @@ function Pizza(size, topping) {
   this.size = size,
   this.toppings = topping
 }
-var price = 0;
-Pizza.prototype.PriceOfPizza = function() {
-  if (selectedSize === "small") {
-    if (sellectedTopping === "cheese" || sellectedTopping === "veggie") {
+
+Pizza.prototype.getPrice = function() {
+  var price = 0;
+  if (this.size === "small") {
+    if (this.toppings === "cheese" || this.toppings === "veggie") {
       price = 6.39;
-    } else if (sellectedTopping === "chicken") {
+    } else if (this.toppings === "chicken") {
       price = 7.50;
     } else {
       price = 8.00;
     }
-  } else if (selectedSize === "medium") {
-    if (sellectedTopping === "cheese" || sellectedTopping === "veggie") {
+  }
+  else if (this.size === "medium") {
+    if (this.toppings === "cheese" || this.toppings === "veggie") {
       price = 7.39;
-    } else if (sellectedTopping === "chicken") {
+    } else if (this.toppings === "chicken") {
       price = 8.50;
     } else {
       price = 9.00;
     }
-  } else if {
-    if (sellectedTopping === "cheese" || sellectedTopping === "veggie") {
+  }
+  else if (this.size === "large") {
+    if (this.toppings === "cheese" || this.toppings === "veggie") {
       price = 8.39;
-    } else if (sellectedTopping === "chicken") {
+    }  else if (this.toppings === "chicken") {
       price = 9.50;
-    } else {
+    }  else {
       price = 10.00;
     }
   }
+  return price;
 }
-
 
 // User Interface Logic
 $(document).ready(function() {
   $("form#pizza").submit(function(event) {
     event.preventDefault();
     var selectedSize = $("#size").attr('selected',true).val();
-    var sellectedTopping = $("input:radio[name=topping]:checked").val();
+    var selectedTopping = $("input:radio[name=topping]:checked").val();
 
     $("#size").val("");
     $("input:radio[name=topping]:checked").val("");
 
-    var newPizza = new Pizza(selectedSize, sellectedTopping);
+    var newPizza = new Pizza(selectedSize, selectedTopping);
 
+    $("#show-price").show();
+    $(".size").html(selectedSize);
+    $(".topping").html(selectedTopping);
 
+    var price = newPizza.getPrice();
+    $("#output").text(price);
   });
 });
