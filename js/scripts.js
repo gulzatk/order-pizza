27428c2eb2333674f6c84f8c03079e04b81ba4fa
Gulzat Karimova <gulzat.karimova@gmail.com>
2018-11-02 13:42:15 -0700

    It will update a file to add checkbox to html

--- js/scripts.js
+++ js/scripts.js
@@ -7,30 +7,31 @@ function Pizza(size, topping) {
 Pizza.prototype.getPrice = function() {
   var price = 0;
   if (this.size === "Small") {
-    if (this.toppings === "cheese" || this.toppings === "veggie") {
-      price = 6.39;
-    } else if (this.toppings === "chicken") {
-      price = 7.50;
-    } else {
+    if (this.toppings.length <= 2) {
+      price = 5.00;
+    } else if (this.toppings.length ===3) {
+      price = 6.50;
+    }
+    else {
       price = 8.17;
     }
   }
   else if (this.size === "Medium") {
-    if (this.toppings === "cheese" || this.toppings === "veggie") {
-      price = 7.39;
-    } else if (this.toppings === "chicken") {
-      price = 8.50;
-    } else {
-      price = 9.00;
+    if (this.toppings.length <= 2) {
+      price = 6.00;
+    } else if (this.toppings.length ===3) {
+      price = 7.19;
+    } else  {
+      price = 9.17;
     }
   }
   else if (this.size === "Large") {
-    if (this.toppings === "cheese" || this.toppings === "veggie") {
-      price = 8.39;
-    }  else if (this.toppings === "chicken") {
-      price = 9.50;
-    }  else {
-      price = 10.68;
+    if (this.toppings.length <= 2) {
+      price = 7.12;
+    } else if (this.toppings.length ===3) {
+      price = 8.00;
+    } else {
+      price = 10.39;
     }
   }
   return price;
@@ -41,16 +42,21 @@ $(document).ready(function() {
   $("form#pizza").submit(function(event) {
     event.preventDefault();
     var selectedSize = $("#size").attr('selected',true).val();
-    var selectedTopping = $("input:radio[name=topping]:checked").val();
+
+    var toppingList = [];
+    $("input:checkbox[name=topping]:checked").each(function(){
+       var selectedToppings = $(this).val();
+       toppingList.push(selectedToppings);
+     });
 
     $("#size").val("");
-    $("input:radio[name=topping]:checked").val("");
 
-    var newPizza = new Pizza(selectedSize, selectedTopping);
+    var newPizza = new Pizza(selectedSize, toppingList);
+    var toppingString = toppingList.toString();
 
     $("#show-price").show();
     $(".size").html(selectedSize);
-    $(".topping").html(selectedTopping);
+    $(".toppings").html(toppingString);
 
     var price = newPizza.getPrice();
     $("#output").html("Your total price is: $" + price);
