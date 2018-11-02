a705e202fa2fed88923ada0458ce5ddb9daf7363
Gulzat Karimova <gulzat.karimova@gmail.com>
2018-11-02 11:36:25 -0700

    It will updated the file to add statements to the js

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,33 @@ function Pizza(size, topping) {
   this.size = size,
   this.toppings = topping
 }
-
+var price = 0;
 Pizza.prototype.PriceOfPizza = function() {
-
+  if (selectedSize === "small") {
+    if (sellectedTopping === "cheese" || sellectedTopping === "veggie") {
+      price = 6.39;
+    } else if (sellectedTopping === "chicken") {
+      price = 7.50;
+    } else {
+      price = 8.00;
+    }
+  } else if (selectedSize === "medium") {
+    if (sellectedTopping === "cheese" || sellectedTopping === "veggie") {
+      price = 7.39;
+    } else if (sellectedTopping === "chicken") {
+      price = 8.50;
+    } else {
+      price = 9.00;
+    }
+  } else if {
+    if (sellectedTopping === "cheese" || sellectedTopping === "veggie") {
+      price = 8.39;
+    } else if (sellectedTopping === "chicken") {
+      price = 9.50;
+    } else {
+      price = 10.00;
+    }
+  }
 }
 
 
@@ -19,7 +43,7 @@ $(document).ready(function() {
     $("#size").val("");
     $("input:radio[name=topping]:checked").val("");
 
-    var newPizza = new Pizza(selectedSize, sellectedTopping, inputtedTime);
+    var newPizza = new Pizza(selectedSize, sellectedTopping);
 
 
   });
